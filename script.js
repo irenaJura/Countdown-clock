d30ce0d74bff46d63bfc100eeedd0c7ece2ad454
Irena Jurasek <irenaskydive@gmail.com>
2020-01-19 16:13:44 +0100

    fix spacing

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
-    const display = `${minutes}: ${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
     timerDisplay.textContent = display;
     document.title = display; // show in tab title
     // console.log(minutes, remainderSeconds);
