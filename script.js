1de4a8734f87a73d4b14a986c5bb9011649c6d47
Irena Jurasek <irenaskydive@gmail.com>
2020-01-19 15:19:11 +0100

    add display end time function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let countdown; // store setInterval
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 // Date.now() returns the number of milliseconds elapsed 
 // since January 1, 1970 00:00:00 UTC
@@ -7,6 +8,7 @@ function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000; // when the time stops
     displayTimeLeft(seconds);
+    displayEndTime(then);
 
     // repeatedly calls a function or executes a code snippet, 
     // with a fixed time delay between each call
@@ -28,4 +30,11 @@ function displayTimeLeft(seconds) {
     timerDisplay.textContent = display;
     document.title = display; // show in tab title
     // console.log(minutes, remainderSeconds);
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp); // pass in the number of milliseconds since UNIX epoch
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
